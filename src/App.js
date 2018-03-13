184e88091e6b0fb558cde67431db910973dc848e
Ghan <87141877@qq.com>
2018-03-13 19:05:54 +0800

    利用create-react-app增加适配配置 完成适配调研

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div w-188-246="w-188-246" className="item"></div>
+        <div w-187-246="w-187-246" className="item"></div>
+        <div w-188-246="w-188-246" className="item"></div>
+        <div w-187-246="w-187-246" className="item">1</div>
+      </div>
+    );
+  }
+}
+
+export default App;
